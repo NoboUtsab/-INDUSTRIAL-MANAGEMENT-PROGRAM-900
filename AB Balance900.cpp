98d49c5a993a07ce17893471e627e3083b1c8d59
NoboUtsab <134442520+NoboUtsab@users.noreply.github.com>
2023-06-25 02:49:47 +0600

    Add files via upload

---
+++ AB Balance900.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    string s;
+        cin>>s;
+        if(s[0]==s[s.size()-1])
+        {
+            cout<<s<<endl;
+        }
+        else
+        {
+            s[s.size()-1]=s[0];
+            cout<<s<<endl;
+        }
+
+
+    }
+}
